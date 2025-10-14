280cdb635d23afbe14d5950600de01bea1c30d58
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2025-10-14 16:04:14 +0300

    refactor: simplify permission logic in IsAdminOrReadOnly class
    - Remove unnecessary checks for user authentication and staff status.
    - Streamline permission handling to allow safe methods for all users.

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -2,13 +2,8 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
 
         if request.user.is_superuser:
             return True
 
-        if request.user.is_staff:
-            return request.method in permissions.SAFE_METHODS
-
-        return False
+        return request.method in permissions.SAFE_METHODS
